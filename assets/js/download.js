491da0486cf1bdd3b834e77dd5023a983fd3dcca
Sandip Olee <52390136+sandipolee@users.noreply.github.com>
2021-05-23 01:05:14 +0545

    new line added

--- assets/js/download.js
+++ assets/js/download.js
@@ -1,48 +1,3 @@
-function createNode(element) {
-    return document.createElement(element);
-}
-
-function append(parent, el) {
-    return parent.appendChild(el);
-}
-
-const ul = document.getElementById('newa');
-// const url = 'http://quotes.rest/quote/random?language=en&limit=1';
-
-// fetch(url)
-//     .then((resp) => resp.json())
-//     .then(function(data) {
-//         let authors = data.results;
-//         console.log(authors);
-//         return authors.map(function(author) {
-//             let li = createNode('li');
-//             let img = createNode('p');
-//             let span = createNode('span');
-//             img.innerHTML = author.contents.quotes.quote;
-//             span.innerHTML = author.contents.quotes.author;
-//             append(li, img);
-//             append(li, span);
-//             append(ul, li);
-//         })
-//     })
-//     .catch(function(error) {
-//         console.log(error);
-//     });
-
-
-mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() { scrollFunction() };
-
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        mybutton.style.display = "block";
-    } else {
-        mybutton.style.display = "none";
-    }
-}
-
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
     document.body.scrollTop = 0; // For Safari
@@ -50,46 +5,46 @@ function topFunction() {
 }
 
 let n = 0;
+check()
 
-fetch("https://type.fit/api/quotes")
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(data) {
-        console.log(data);
-        prinrthis(data)
-    });
+function check() {
+    if (n === 0) {
+        document.getElementById("pre").setAttribute("class", "btn btn-primary col-md-2 m-auto  mr-auto ml-auto mt-3 disabled")
+    } else {
+        document.getElementById("pre").setAttribute("class", "btn btn-primary col-md-2 m-auto mr-auto ml-auto mt-3 ")
+    }
+}
 
 
+function fetchData() {
+    fetch("https://type.fit/api/quotes")
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+            prinrthis(data)
+        });
+}
+
+
+fetchData()
 
 function next(vdata) {
 
     n = n + 10;
 
-    fetch("https://type.fit/api/quotes")
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(data) {
-            console.log("next");
-            prinrthis(data)
-        });
+    fetchData()
     topFunction()
-
+    check()
 
 }
 
 function pre(pdata) {
     n = n - 10;
-    fetch("https://type.fit/api/quotes")
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(data) {
-
-            prinrthis(data)
-        });
+    fetchData()
     topFunction()
+    check()
 }
 
 function prinrthis(dataQ) {

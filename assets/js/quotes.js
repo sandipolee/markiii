48fe07bf145e49076b3d67be585c3f413be39f69
Sandip Olee <52390136+sandipolee@users.noreply.github.com>
2021-05-23 11:47:45 +0545

    new

--- assets/js/quotes.js
+++ assets/js/quotes.js
@@ -1,35 +1,65 @@
-fetch("assets/json/quotes.json")
-    .then(function(resp) {
-        return resp.json();
-    })
-    .then(function(data) {
-        quotes(data);
-    })
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
 
-function quotes(qdata) {
-    let quotesv = qdata['quotes'];
-    let fealdtext = document.getElementById('row-box');
-    for (let i = 0; i < quotesv.length; i++) {
-        fealdtext.innerHTML += `<div onclick="copyquote(` + i + `)" class="h-post card p-3 mt-4 col-md-5 ">
-        <h5 >` + quotesv[i]['quote'] + `</h5>
+let n = 0;
+check()
 
-        <p class="text-info">` + quotesv[i]['author'] + `</p>
-    </div>`
+function check() {
+    if (n === 0) {
+        document.getElementById("pre").setAttribute("class", "btn btn-primary col-md-2 m-auto  mr-auto ml-auto mt-3 disabled")
+    } else {
+        document.getElementById("pre").setAttribute("class", "btn btn-primary col-md-2 m-auto mr-auto ml-auto mt-3")
     }
 }
 
-//  --------------------------------------------
-//       ******** copy function ******
-// -----------------------------------------------
 
-function copyquote(data) {
+function fetchData() {
+    fetch("https://type.fit/api/quotes")
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+            prinrthis(data)
+        });
+}
 
-    data.select();
-    copyText.setSelectionRange(0, 99999); /* For mobile devices */
 
-    /* Copy the text inside the text field */
-    document.execCommand("copy");
+fetchData()
 
-    /* Alert the copied text */
-    alert("Copied ");
+function next(vdata) {
+
+    n = n + 10;
+
+    fetchData()
+    topFunction()
+    check()
+
+}
+
+function pre(pdata) {
+    n = n - 10;
+    fetchData()
+    topFunction()
+    check()
+}
+
+function prinrthis(dataQ) {
+    const ul = document.getElementById('newa');
+    ul.innerHTML = "";
+    ul.innerHTML += `<p>${dataQ.length}</p>`;
+    for (let i = n; i < n + 10; i++) {
+
+        ul.innerHTML += `<div>
+        <p class="text-info" >${i+1}</p>
+        <p >${dataQ[i]['text']}</p>
+        <p class="text-success">${dataQ[i]['author']}</p>
+        <hr>
+         </div>`
+    }
+
+
 }
